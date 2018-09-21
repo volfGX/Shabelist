be3d302ba14a7e4566a7a87be8b65fab4b49cfee
kajumaru <girigiriwakkusi@outlook.jp>
2018-09-21 07:19:30 +0000

    paginate

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
   def index
-    @messages = Message.all
+    @messages = Message.order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def show
